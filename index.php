d8308c93d6493eb42d746527d2575147c765955a
ruzamca <ruzamca@gmail.com>
2018-04-14 16:29:58 +0200

    actualizado el css de los 3 index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
--- php/index/padre.php
+++ php/index/padre.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ruben
- * Date: 14/04/2018
- * Time: 12:28
- */
+    session_start();
+
+    if(!isset($_SESSION{'usuario'})){
+        header("Location: ../../index.php");
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="../../css/header-index.css">
+</head>
+<body>
+<div id="main">
+    <div id="header">
+        <a href="../../index.php">
+            <div class="divlogo">
+                <img src="../../img/logo-cazada.png">
+            </div>
+            <div class="divlogoname">
+                <span>CAZADA</span><span>Gestor escolar</span>
+            </div>
+        </a>
+        <div class="typepanel">
+            <p><span>Panel</span><span>MADRE/PADRE/TUTOR</span></p>
+        </div>
+    </div>
+    <div id="content">
+
+    </div>
+    <div id="footer">
+
+    </div>
+</div>
+</body>
+</html>
