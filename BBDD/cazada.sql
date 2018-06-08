52cc187a6e279a5536ea9a4294be839e00a3446a
kaibano <kaibano22@gmail.com>
2018-06-08 13:12:56 +0200

    update cañi

--- BBDD/cazada.sql
+++ BBDD/cazada.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2018 a las 18:10:11
+-- Tiempo de generación: 08-06-2018 a las 13:11:52
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.1
 
@@ -49,19 +49,57 @@ INSERT INTO `administradores` (`Usuario`, `Password`, `Mail`) VALUES
 
 CREATE TABLE `alumnos` (
   `ID_Alumno` int(5) NOT NULL,
-  `ID_Clase` int(11) NOT NULL,
+  `ID_Clase` int(11) DEFAULT NULL,
   `Padre` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `Nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
-  `Apellidos` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
-  `Asignaturas` varchar(400) COLLATE utf8_spanish_ci NOT NULL
+  `Apellidos` varchar(80) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `alumnos`
 --
 
-INSERT INTO `alumnos` (`ID_Alumno`, `ID_Clase`, `Padre`, `Nombre`, `Apellidos`, `Asignaturas`) VALUES
-(5942, 1, '49268543X', 'Jacinto', 'perez Pacuito', '1 2 3 4 8 9 10 11 12');
+INSERT INTO `alumnos` (`ID_Alumno`, `ID_Clase`, `Padre`, `Nombre`, `Apellidos`) VALUES
+(1, 1, '49268543X', 'Jacinto', 'Perez Pacuito'),
+(2, 1, '49268544X', 'Ramon', 'Casas Martinez'),
+(3, 1, '49268545X', 'Carlos', 'Ramon Sanchez'),
+(4, 1, '49268546X', 'Juan', 'Buenavida Pascual'),
+(5, 1, '49268547X', 'Raul', 'Martin Perez'),
+(6, 1, '49268548X', 'Ruben', 'Perez Santiago'),
+(7, 1, '49268549X', 'David', 'Carmelo Blanco'),
+(8, 1, '49268540X', 'Daniel', 'Carrero Humanes'),
+(9, 1, '49268550X', 'Fernando', 'Sol Luz'),
+(10, 1, '49268553X', 'Francisco', 'Felipe Santos'),
+(11, 2, '49268552X', 'Manuel', 'Carro Gomez'),
+(12, 2, '23456270A', 'Perico', 'Gomez Sierra'),
+(13, 2, '23456271A', 'Miguel', 'Diaz Moreno'),
+(14, 2, '23456272A', 'Laura', 'Diaz Cordobes'),
+(15, 2, '23456273A', 'Paula', 'Garcia Conde'),
+(16, 2, '23456274A', 'Daniel', 'Media Gonzalez'),
+(17, 2, '23456275A', 'Laura', 'Sierra Gomez'),
+(18, 2, '23456276A', 'Rafael', 'Leon Pasas'),
+(19, 2, '23456277A', 'Manuel', 'Pulido Martin'),
+(20, 2, '23456278A', 'Dafne', 'Zamora Serrano'),
+(21, 3, '12345670B', 'Maria', 'Parreno Gonzalez'),
+(22, 3, '12345671B', 'Silvia', 'Mora Parra'),
+(23, 3, '12345672B', 'Juan Luis', 'Guerra Pozo'),
+(24, 3, '12345673B', 'Victor', 'Pozo Rosa'),
+(25, 3, '12345674B', 'Sergio', 'Aguero Moran'),
+(26, 3, '12345675B', 'Pablo', 'Lucas Mora'),
+(27, 3, '12345676B', 'Natalia', 'Sierra Zamora'),
+(28, 3, '12345677B', 'Francisco', 'Gomez Martin'),
+(29, 3, '12345678B', 'Francisca', 'Parra Gonzalez'),
+(30, 3, '12345679B', 'Cesar', 'Rubio Pino'),
+(31, 4, '48902341C', 'Paula', 'Monte Pino'),
+(32, 4, '48902342C', 'Sara', 'Segovia Leon'),
+(33, 4, '48902343C', 'Paula', 'Cintas Gomez'),
+(34, 4, '48902344C', 'Sergio', 'Gil Gil'),
+(35, 4, '48902345C', 'Alvaro', 'Gonzalez Moreira'),
+(36, 4, '48902346C', 'Ruben', 'Martinez Higado'),
+(37, 4, '48902347C', 'Jose Manuel', 'Moreno Garcia'),
+(38, 4, '48902348C', 'David', 'Fernandez Lloreda'),
+(39, 4, '48902349C', 'Esther', 'Fernandez Toro'),
+(40, 4, '48902340C', 'Ivan', 'Alonso Vidal');
 
 -- --------------------------------------------------------
 
@@ -109,8 +147,10 @@ CREATE TABLE `clases` (
 --
 
 INSERT INTO `clases` (`ID_Clase`, `Tutor`, `Clase`) VALUES
-(1, '74125968H', '1A - ESO'),
-(2, '96325481D', '2A - ESO');
+(1, '00000003A', '1A - ESO'),
+(2, '00000006A', '2A - ESO'),
+(3, '00000009A', '3A - ESO'),
+(4, '00000012A', '4A - ESO');
 
 -- --------------------------------------------------------
 
@@ -143,8 +183,8 @@ CREATE TABLE `horarios` (
   `Clase` int(11) NOT NULL,
   `Dia` int(1) NOT NULL,
   `Hora` int(1) NOT NULL,
-  `Asignatura` int(11) NOT NULL,
-  `Profesor` varchar(10) COLLATE utf8_spanish_ci NOT NULL
+  `Asignatura` int(11) DEFAULT NULL,
+  `Profesor` varchar(10) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -152,12 +192,126 @@ CREATE TABLE `horarios` (
 --
 
 INSERT INTO `horarios` (`Clase`, `Dia`, `Hora`, `Asignatura`, `Profesor`) VALUES
-(1, 1, 1, 1, '23569874K'),
-(1, 1, 2, 2, '41203587T'),
-(1, 1, 3, 3, '74125968H'),
-(1, 1, 4, 4, '85246781D'),
-(1, 1, 5, 5, '96321548S'),
-(1, 1, 6, 8, '96325481D');
+(1, 1, 1, 1, '00000001A'),
+(1, 1, 2, 2, '00000002A'),
+(1, 1, 3, 3, '00000009A'),
+(1, 1, 4, 4, '00000004A'),
+(1, 1, 5, 5, '00000011A'),
+(1, 1, 6, 6, '00000006A'),
+(1, 2, 1, 1, '00000001A'),
+(1, 2, 2, 3, '00000009A'),
+(1, 2, 3, 4, '00000004A'),
+(1, 2, 4, 5, '00000011A'),
+(1, 2, 5, 6, '00000006A'),
+(1, 2, 6, 2, '00000002A'),
+(1, 3, 1, 1, '00000001A'),
+(1, 3, 2, 4, '00000004A'),
+(1, 3, 3, 5, '00000011A'),
+(1, 3, 4, 6, '00000006A'),
+(1, 3, 5, 2, '00000002A'),
+(1, 3, 6, 3, '00000009A'),
+(1, 4, 1, 1, '00000001A'),
+(1, 4, 2, 5, '00000011A'),
+(1, 4, 3, 6, '00000006A'),
+(1, 4, 4, 2, '00000002A'),
+(1, 4, 5, 3, '00000009A'),
+(1, 4, 6, 4, '00000004A'),
+(1, 5, 1, 1, '00000001A'),
+(1, 5, 2, 6, '00000006A'),
+(1, 5, 3, 2, '00000002A'),
+(1, 5, 4, 3, '00000009A'),
+(1, 5, 5, 4, '00000004A'),
+(1, 5, 6, 5, '00000011A'),
+(2, 1, 1, 1, '00000007A'),
+(2, 1, 2, 2, '00000003A'),
+(2, 1, 3, 3, '00000003A'),
+(2, 1, 4, 4, '00000010A'),
+(2, 1, 5, 5, '00000005A'),
+(2, 1, 6, 6, '00000002A'),
+(2, 2, 1, 2, '00000002A'),
+(2, 2, 2, 3, '00000003A'),
+(2, 2, 3, 4, '00000010A'),
+(2, 2, 4, 5, '00000005A'),
+(2, 2, 5, 6, '00000010A'),
+(2, 2, 6, 1, '00000007A'),
+(2, 3, 1, 3, '00000003A'),
+(2, 3, 2, 4, '00000010A'),
+(2, 3, 3, 5, '00000005A'),
+(2, 3, 4, 6, '00000011A'),
+(2, 3, 5, 1, '00000001A'),
+(2, 3, 6, 2, '00000008A'),
+(2, 4, 1, 4, '00000010A'),
+(2, 4, 2, 5, '00000005A'),
+(2, 4, 3, 6, '00000012A'),
+(2, 4, 4, 1, '00000007A'),
+(2, 4, 5, 2, '00000006A'),
+(2, 4, 6, 3, '00000003A'),
+(2, 5, 1, 5, '00000005A'),
+(2, 5, 2, 6, '00000004A'),
+(2, 5, 3, 1, '00000007A'),
+(2, 5, 4, 2, '00000007A'),
+(2, 5, 5, 3, '00000001A'),
+(2, 5, 6, 4, '00000008A'),
+(3, 1, 1, 2, '00000008A'),
+(3, 1, 2, 3, '00000009A'),
+(3, 1, 3, 4, '00000004A'),
+(3, 1, 4, 5, '00000011A'),
+(3, 1, 5, 6, '00000012A'),
+(3, 1, 6, 1, '00000001A'),
+(3, 2, 1, 3, '00000009A'),
+(3, 2, 2, 4, '00000004A'),
+(3, 2, 3, 5, '00000011A'),
+(3, 2, 4, 6, '00000012A'),
+(3, 2, 5, 1, '00000001A'),
+(3, 2, 6, 2, '00000008A'),
+(3, 3, 1, 4, '00000004A'),
+(3, 3, 2, 5, '00000011A'),
+(3, 3, 3, 6, '00000012A'),
+(3, 3, 4, 1, '00000001A'),
+(3, 3, 5, 2, '00000008A'),
+(3, 3, 6, 3, '00000004A'),
+(3, 4, 1, 5, '00000011A'),
+(3, 4, 2, 6, '00000012A'),
+(3, 4, 3, 1, '00000001A'),
+(3, 4, 4, 2, '00000008A'),
+(3, 4, 5, 3, '00000001A'),
+(3, 4, 6, 4, '00000008A'),
+(3, 5, 1, 6, '00000012A'),
+(3, 5, 2, 1, '00000001A'),
+(3, 5, 3, 2, '00000008A'),
+(3, 5, 4, 3, '00000012A'),
+(3, 5, 5, 4, '00000009A'),
+(3, 5, 6, 5, '00000007A'),
+(4, 1, 1, 3, '00000003A'),
+(4, 1, 2, 4, '00000010A'),
+(4, 1, 3, 5, '00000005A'),
+(4, 1, 4, 6, '00000012A'),
+(4, 1, 5, 1, '00000007A'),
+(4, 1, 6, 2, '00000008A'),
+(4, 2, 1, 4, '00000010A'),
+(4, 2, 2, 5, '00000005A'),
+(4, 2, 3, 6, '00000012A'),
+(4, 2, 4, 1, '00000007A'),
+(4, 2, 5, 2, '00000008A'),
+(4, 2, 6, 3, '00000003A'),
+(4, 3, 1, 5, '00000005A'),
+(4, 3, 2, 6, '00000012A'),
+(4, 3, 3, 1, '00000007A'),
+(4, 3, 4, 2, '00000008A'),
+(4, 3, 5, 3, '00000003A'),
+(4, 3, 6, 4, '00000010A'),
+(4, 4, 1, 6, '00000012A'),
+(4, 4, 2, 1, '00000007A'),
+(4, 4, 3, 2, '00000008A'),
+(4, 4, 4, 3, '00000003A'),
+(4, 4, 5, 4, '00000010A'),
+(4, 4, 6, 5, '00000005A'),
+(4, 5, 1, 1, '00000007A'),
+(4, 5, 2, 2, '00000008A'),
+(4, 5, 3, 3, '00000003A'),
+(4, 5, 4, 4, '00000010A'),
+(4, 5, 5, 5, '00000005A'),
+(4, 5, 6, 6, '00000012A');
 
 -- --------------------------------------------------------
 
@@ -221,7 +375,7 @@ CREATE TABLE `padres` (
   `Mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `Nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `Apellidos` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
-  `Alumnos` varchar(400) COLLATE utf8_spanish_ci NOT NULL
+  `Alumnos` varchar(8) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -229,7 +383,46 @@ CREATE TABLE `padres` (
 --
 
 INSERT INTO `padres` (`Usuario`, `Password`, `Mail`, `Nombre`, `Apellidos`, `Alumnos`) VALUES
-('49268543X', '1234', 'padre1@gmail.com', 'Papa', 'papito papi', '5942');
+('49268543X', '1234', 'padre1@gmail.com', 'Papa', 'papito papi', '1'),
+('49268553X', '1234', 'padre10@gmail.com', 'Papa', 'papito papi', '10'),
+('49268552X', '1234', 'padre11@gmail.com', 'Papa', 'papito papi', '11'),
+('23456270A', '1234', 'padre12@gmail.com', 'Papa', 'papito papi', '12'),
+('23456271A', '1234', 'padre13@gmail.com', 'Papa', 'papito papi', '13'),
+('23456272A', '1234', 'padre14@gmail.com', 'Papa', 'papito papi', '14'),
+('23456273A', '1234', 'padre15@gmail.com', 'Papa', 'papito papi', '15'),
+('23456274A', '1234', 'padre16@gmail.com', 'Papa', 'papito papi', '16'),
+('23456275A', '1234', 'padre17@gmail.com', 'Papa', 'papito papi', '17'),
+('23456276A', '1234', 'padre18@gmail.com', 'Papa', 'papito papi', '18'),
+('23456277A', '1234', 'padre19@gmail.com', 'Papa', 'papito papi', '19'),
+('49268544X', '1234', 'padre2@gmail.com', 'Papa', 'papito papi', '2'),
+('23456278A', '1234', 'padre20@gmail.com', 'Papa', 'papito papi', '20'),
+('12345670B', '1234', 'padre21@gmail.com', 'Papa', 'papito papi', '21'),
+('12345671B', '1234', 'padre22@gmail.com', 'Papa', 'papito papi', '22'),
+('12345672B', '1234', 'padre23@gmail.com', 'Papa', 'papito papi', '23'),
+('12345673B', '1234', 'padre24@gmail.com', 'Papa', 'papito papi', '24'),
+('12345674B', '1234', 'padre25@gmail.com', 'Papa', 'papito papi', '25'),
+('12345675B', '1234', 'padre26@gmail.com', 'Papa', 'papito papi', '26'),
+('12345676B', '1234', 'padre27@gmail.com', 'Papa', 'papito papi', '27'),
+('12345677B', '1234', 'padre28@gmail.com', 'Papa', 'papito papi', '28'),
+('12345678B', '1234', 'padre29@gmail.com', 'Papa', 'papito papi', '29'),
+('49268545X', '1234', 'padre3@gmail.com', 'Papa', 'papito papi', '3'),
+('12345679B', '1234', 'padre30@gmail.com', 'Papa', 'papito papi', '30'),
+('48902341C', '1234', 'padre31@gmail.com', 'Papa', 'papito papi', '31'),
+('48902342C', '1234', 'padre32@gmail.com', 'Papa', 'papito papi', '32'),
+('48902343C', '1234', 'padre33@gmail.com', 'Papa', 'papito papi', '33'),
+('48902344C', '1234', 'padre34@gmail.com', 'Papa', 'papito papi', '34'),
+('48902345C', '1234', 'padre35@gmail.com', 'Papa', 'papito papi', '35'),
+('48902346C', '1234', 'padre36@gmail.com', 'Papa', 'papito papi', '36'),
+('48902347C', '1234', 'padre37@gmail.com', 'Papa', 'papito papi', '37'),
+('48902348C', '1234', 'padre38@gmail.com', 'Papa', 'papito papi', '38'),
+('48902349C', '1234', 'padre39@gmail.com', 'Papa', 'papito papi', '39'),
+('49268546X', '1234', 'padre4@gmail.com', 'Papa', 'papito papi', '4'),
+('48902340C', '1234', 'padre40@gmail.com', 'Papa', 'papito papi', '40'),
+('49268547X', '1234', 'padre5@gmail.com', 'Papa', 'papito papi', '5'),
+('49268548X', '1234', 'padre6@gmail.com', 'Papa', 'papito papi', '6'),
+('49268549X', '1234', 'padre7@gmail.com', 'Papa', 'papito papi', '7'),
+('49268540X', '1234', 'padre8@gmail.com', 'Papa', 'papito papi', '8'),
+('49268550X', '1234', 'padre9@gmail.com', 'Papa', 'papito papi', '9');
 
 -- --------------------------------------------------------
 
@@ -241,8 +434,7 @@ CREATE TABLE `profesores` (
   `Usuario` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `Password` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `Mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `Nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
-  `Apellidos` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `Nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `Tutoria` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -250,13 +442,19 @@ CREATE TABLE `profesores` (
 -- Volcado de datos para la tabla `profesores`
 --
 
-INSERT INTO `profesores` (`Usuario`, `Password`, `Mail`, `Nombre`, `Apellidos`, `Tutoria`) VALUES
-('23569874K', '1234', 'pro@gmail.com', 'Agustin', 'Hidalgo Ramirez', NULL),
-('41203587T', '1234', 'profesora@gmail.com', 'Elena', 'Vazquez Bernal', NULL),
-('74125968H', '1234', 'profe@gmail.com', 'Profee', 'Maestro teacher', 1),
-('85246781D', '1234', 'teacher@hotmail.com', 'Tomas', 'Linda Soriano', NULL),
-('96321548S', '1234', 'profess@gmail.com', 'Paloma', 'Estaban Garrido', NULL),
-('96325481D', '1234', 'profe2@gmail.com', 'Manuela', 'Velasco Zorrilla', 2);
+INSERT INTO `profesores` (`Usuario`, `Password`, `Mail`, `Nombre`, `Tutoria`) VALUES
+('00000001A', '1234', 'profe1@gmail.com', 'Agustin Hidalgo Ramirez', NULL),
+('00000002A', '1234', 'profe2@gmail.com', 'Elena Vazquez Bernal', NULL),
+('00000003A', '1234', 'profe3@gmail.com', 'Manuel Elias Perez', 1),
+('00000004A', '1234', 'profe4@hotmail.com', 'Tomas Linda Soriano', NULL),
+('00000005A', '1234', 'profe5@gmail.com', 'Paloma Estaban Garrido', NULL),
+('00000006A', '1234', 'profe6@gmail.com', 'Manuela Velasco Zorrilla', 2),
+('00000007A', '1234', 'profe7@gmail.com', 'Claudio Romero De Torres', NULL),
+('00000008A', '1234', 'profe8@gmail.com', 'Ilenia Gonzalez Martinez', NULL),
+('00000009A', '1234', 'profe9@gmail.com', 'Jose Ponze De Leon', 3),
+('00000010A', '1234', 'profe10@hotmail.com', 'Mari Toldo Ramirez', NULL),
+('00000011A', '1234', 'profe11@gmail.com', 'Juan Perez De Dios', NULL),
+('00000012A', '1234', 'profe12@gmail.com', 'Aitor Ortega Perez', 4);
 
 --
 -- Índices para tablas volcadas
@@ -272,7 +470,7 @@ ALTER TABLE `administradores`
 -- Indices de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  ADD PRIMARY KEY (`ID_Alumno`,`ID_Clase`),
+  ADD PRIMARY KEY (`ID_Alumno`),
   ADD KEY `ID_Clase` (`ID_Clase`),
   ADD KEY `Padre` (`Padre`);
 
@@ -300,7 +498,7 @@ ALTER TABLE `faltas`
 -- Indices de la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  ADD PRIMARY KEY (`Clase`,`Hora`),
+  ADD PRIMARY KEY (`Clase`,`Dia`,`Hora`),
   ADD KEY `Hora` (`Hora`),
   ADD KEY `Profesor` (`Profesor`),
   ADD KEY `Asignatura` (`Asignatura`);
@@ -322,7 +520,7 @@ ALTER TABLE `notas`
 -- Indices de la tabla `padres`
 --
 ALTER TABLE `padres`
-  ADD PRIMARY KEY (`Usuario`);
+  ADD PRIMARY KEY (`Alumnos`);
 
 --
 -- Indices de la tabla `profesores`
@@ -345,53 +543,7 @@ ALTER TABLE `asignaturas`
 -- AUTO_INCREMENT de la tabla `clases`
 --
 ALTER TABLE `clases`
-  MODIFY `ID_Clase` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `alumnos`
---
-ALTER TABLE `alumnos`
-  ADD CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`ID_Clase`) REFERENCES `clases` (`ID_Clase`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `alumnos_ibfk_2` FOREIGN KEY (`Padre`) REFERENCES `padres` (`Usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `clases`
---
-ALTER TABLE `clases`
-  ADD CONSTRAINT `clases_ibfk_1` FOREIGN KEY (`Tutor`) REFERENCES `profesores` (`Usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `faltas`
---
-ALTER TABLE `faltas`
-  ADD CONSTRAINT `faltas_ibfk_1` FOREIGN KEY (`ID_Alumno`) REFERENCES `alumnos` (`ID_Alumno`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `faltas_ibfk_2` FOREIGN KEY (`ID_Asig`) REFERENCES `asignaturas` (`ID_Asig`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `horarios`
---
-ALTER TABLE `horarios`
-  ADD CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`Clase`) REFERENCES `clases` (`ID_Clase`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `horarios_ibfk_2` FOREIGN KEY (`Hora`) REFERENCES `horas` (`Hora`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `horarios_ibfk_3` FOREIGN KEY (`Profesor`) REFERENCES `profesores` (`Usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `horarios_ibfk_4` FOREIGN KEY (`Asignatura`) REFERENCES `asignaturas` (`ID_Asig`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `notas`
---
-ALTER TABLE `notas`
-  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`ID_Alumno`,`ID_Clase`) REFERENCES `alumnos` (`ID_Alumno`, `ID_Clase`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notas_ibfk_2` FOREIGN KEY (`ID_Asig`) REFERENCES `asignaturas` (`ID_Asig`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `profesores`
---
-ALTER TABLE `profesores`
-  ADD CONSTRAINT `profesores_ibfk_1` FOREIGN KEY (`Tutoria`) REFERENCES `clases` (`ID_Clase`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `ID_Clase` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
