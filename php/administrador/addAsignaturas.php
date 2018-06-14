652ebd8b8427a2f848a25358db7285d6752b53f3
rcasas21 <ruben.zambrano@decathlon.com>
2018-06-14 10:46:14 +0200

    problemas asignaturas solucionados

--- php/administrador/addAsignaturas.php
+++ php/administrador/addAsignaturas.php
@@ -1,13 +1,20 @@
 <?php
     include "../class/conexiones.php";
 
-    $nombreAsignatura = $_POST['asignatura'];
+    $nombreAsignatura = $_POST['asig'];
 
     $bbdd = new Conexion();
     $conexion = $bbdd->conectar();
-    $resultado = $conexion->query("INSERT INTO asignaturas (Nombre) VALUE ('$nombreAsignatura')");
-    if($resultado){
-        echo "true";
-    }else{
-        echo "false";
+    if($nombreAsignatura === ""){
+        echo "vacio";
+    }else {
+        $res1 = $bbdd->consulta("SELECT * FROM asignaturas WHERE Nombre = '$nombreAsignatura'");
+        if(empty($res1)) {
+            $resultado = $conexion->query("INSERT INTO asignaturas (Nombre) VALUE ('$nombreAsignatura')");
+            if ($resultado) {
+                echo "true";
+            }
+        }else{
+            echo "existe";
+        }
     }
